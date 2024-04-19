14184e9af986e49e4840e39923c821edd997a807
sbssdeveloper <saibhaskarmail@gmail.com>
2024-04-19 15:03:38 +0530

    Updated astro file with modifications

--- astro.php
+++ astro.php
@@ -1,7 +1,14 @@
 <?php
-function addition1($a, $b) {
-    return $a + $b;
+function addition1($a,$b,$c) {
+    return $a + $b+$c;
 }
 
-$c = addition1(2, 3);
+$c = addition1(2, 3,4);
 echo $c;
+
+function multiplication($a,$b,$c) {
+    return $a*$b*$c;
+}
+
+$d = multiplication(2, 3,4);
+echo $d;
